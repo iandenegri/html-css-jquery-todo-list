72c0b0ea5ab80349aa6786b39569922c782d5db5
Ian Denegri <iandenegri@hotmail.com>
2019-07-04 21:17:43 -0400

    first commit

---
+++ assets/js/todo.js
@@ -0,0 +1,26 @@
+// Cross out to-do items by clicking
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+// Click X to delete to-do item
+$("ul").on("click", "span", function(event){
+    // Remove the li holding this span after a 400ms fadeout
+    $(this).parent().fadeOut(400, function(){
+        $(this).remove();
+    });
+    // Events in any parent objects will not occur after this interacting with the span.
+    event.stopPropagation();
+});
+
+// Add new to-do item to list on enter press
+$("input[type='text']").keypress(function(event){
+    // if keypress is enter (enter val is 13) and the todo item isnt blank
+    if (event.which === 13 && $(this).val() !== ""){
+        // assign the current text in input to var
+        let newTodo = $(this).val();
+        $(this).val("");
+        // create new li and add to ul
+        $("ul").append("<li><span>X</span> " + newTodo + "</li>");
+    }
+});
